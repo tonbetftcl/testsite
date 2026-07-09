b0e9e662222bcfd567111d8f0282e746aad28c36
tonbetftcl <diamondtestbp@gmail.com>
2026-07-09 12:39:00 +0400

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyCG1FHpgPg3xC9IHZ0SzaPn0Ngxk8IbwS4",
+    authDomain: "tonbetftcl.firebaseapp.com",
+    databaseURL: "https://tonbetftcl-default-rtdb.firebaseio.com",
+    projectId: "tonbetftcl",
+    storageBucket: "tonbetftcl.firebasestorage.app",
+    messagingSenderId: "683608892991",
+    appId: "1:683608892991:web:bd70872ad2f130b8a3ba8a"
+});
